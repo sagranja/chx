968dc18834a5d60bb6d2a388e7caf57ff4e1a5ec
grel4d <greer.relphorde@gmail.com>
2019-03-14 18:33:08 +0000

    CRUD for a Game Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_033808) do
+ActiveRecord::Schema.define(version: 2019_03_14_175509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.integer "white_player"
+    t.integer "black_player"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
