8f7e151e65da8661f8ccf0d9839e964fb5911fe9
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-03-28 01:28:00 +0000

    "Modifiying FactoryBot to new format add_attribute and install factory bot"
    Conflicts:
    	spec/models/game_spec.rb

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,14 +1,18 @@
 FactoryBot.define do
-
   factory :user do
     sequence :email do |n|
-      { "dummyEmail#{n}@gmail.com" }
+      "dummyEmail#{n}@gmail.com"
     end
-    password { "secretPassword" }
-    password_confirmation { "secretPassword" }
+
+    add_attribute(:password) { 'secretPassword' }
+    add_attribute(:password_confirmation) { 'secretPassword' }
   end
 
   factory :game do
-    name { "New Game" }
+    add_attribute( :name) { 'New Game' }
+    add_attribute( :white_player) { 1 }
+    add_attribute( :white_player) { 2 }
+    
+    association :user
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
 
+  describe "populate_game" do
+    white_player = FactoryBot.create(:user)
+    black_player = FactoryBot.create(:user)
+
+    game = Game.create(name: "New Game", white_player: white_player, black_player: black_player)
+  end
 end
