6fc996570f9433d9ab507155b746ec55821386fa
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-03-28 01:28:00 +0000

    "Modifiying FactoryBot to new format add_attribute and install factory bot"

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
 
   describe "populate_game" do
-    white_player = create(:user)
-    black_player = create(:user)
+    white_player = FactoryBot.create(:user)
+    black_player = FactoryBot.create(:user)
 
     game = Game.create(name: "New Game", white_player: white_player, black_player: black_player)
-    byebug
+    #byebug
   end
 end
