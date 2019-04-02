4e422769148b7141f77ca5f4430dd93ce95ad61e
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-04-02 03:15:15 +0000

    uninitialized constant Game::Pawn

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
 
   describe "populate_game" do
+
+    it "should populate pieces onto board correctly" 
     game = FactoryBot.create(:game)
     expect(pawn.position_y).to eq(1 || 6)
   end
