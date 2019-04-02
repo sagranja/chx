3158ef0e01af0b49374f475cb4b78b513be0c2f3
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-04-02 03:05:24 +0000

    uninitialized constant Game::Pawn
    Conflicts:
    	spec/models/game_spec.rb
    	spec/support/factory_bot.rb

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,18 +1,8 @@
 FactoryBot.define do
-  factory :user do
-    sequence :email do |n|
-      "dummyEmail#{n}@gmail.com"
-    end
-
-    add_attribute(:password) { 'secretPassword' }
-    add_attribute(:password_confirmation) { 'secretPassword' }
-  end
 
   factory :game do
     add_attribute( :name) { 'New Game' }
-    add_attribute( :white_player) { 1 }
-    add_attribute( :white_player) { 2 }
     
-    association :user
+    # association :user
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
 
   describe "populate_game" do
-    white_player = FactoryBot.create(:user)
-    black_player = FactoryBot.create(:user)
-
-    game = Game.create(name: "New Game", white_player: white_player, black_player: black_player)
+    game = FactoryBot.create(:game)
   end
 end
