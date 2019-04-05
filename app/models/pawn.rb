c4558419066ef73c0832af45613b4ccabf603440
grel4d <greer.relphorde@gmail.com>
2019-04-05 17:50:53 +0000

    plus_rook

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,14 +1,17 @@
 class Pawn < Piece
 
   def valid_move?(x, y) 
-      if position_x != x
-        return false  
-      end
-      if position_y == 1 && y == 3 
-        return true
-      elsif ( y - position_y ) == 1  
-        return true
-      end
+    if self.is_obstructed?(x, y) 
+      return false 
+    end
+    if position_x != x
+      return false  
+    end
+    if position_y == 1 && y == 3 
+      return true
+    elsif ( y - position_y ) == 1  
+      return true
+    end
     return false
   end
   
