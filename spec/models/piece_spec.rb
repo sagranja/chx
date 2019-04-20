ecd74fca001f5e00f79056aabefd13608b45b36a
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-04-20 02:33:09 +0000

    adding tests to spec model for pieces

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+    it "should fail if target is same color" do
+      board = FactoryBot.create(:game)
+      board.pieces.delete_all
+      king = FactoryBot.create(:king)
+      knight = FactoryBot.create(:knight)
+      king.move_to!(2, 3)
+      expect(move_to!).to be false 
+    end
+
+    it "should update attributes and capture target" do
+      board = FactoryBot.create(:game)
+      board.pieces.delete_all
+      king = FactoryBot.create(:king)
+      pawn = FactoryBot.create(:pawn)
+      pawn.move_to!(2, 2)
+      expect(pawn.position_x).to eq(2)
+      expect(pawn.position_y).to eq(2)
+      expect(king.position_x).to be_nil
+      expect(king.position_y).to be_nil
+    end
+  end
+  
 end
