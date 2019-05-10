590cbc4f744231467ec22b9b9432e83749863bcb
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-05-10 00:41:40 +0000

    changed tests in piece spec to locate King accurately

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,26 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  
-    it "should fail if target is same color" do
-      board = FactoryBot.create(:game)
-      king = board.pieces.find_by(position_x: 4,position_y: 0)
-      knight = board.pieces.find_by(position_x:6, position_y: 0)
+  let(:board) { FactoryBot.create(:game) }
+  let(:king) { board.pieces.find_by(type: "King", color: "t") }
+
+  context "when the pieces are the same color" do
+    let(:knight) { board.pieces.find_by(position_x: 6, position_y: 0) }
+
+    it "won't be successful" do
       knight.update_attributes(position_x: 2, position_y: 3)
       king.update_attributes(position_x: 2, position_y: 2)
-      expect(king.move_to!(2, 3)).to be false 
+      expect(king.move_to!(2, 3)).to be false
     end
+  end
 
-    it "should update attributes and capture target" do
-      board = FactoryBot.create(:game)
-      king = board.pieces.find_by(position_x: 4,position_y: 0)
-      pawn = board.pieces.find_by(position_x: 1, position_y: 6)
-      king.update_attributes(position_x: 2, position_y: 2)
-      pawn.update_attributes(position_x: 2, position_y: 3)
+  context "when the pieces are different colors" do
+    let(:pawn) { board.pieces.where(type: "Pawn", color: "f").first }
+
+    it "should update target attributes and capture target" do
+      king.update(position_x: 2, position_y: 2)
+      pawn.update(position_x: 2, position_y: 3)
       pawn.move_to!(2, 2)
       expect(pawn.position_x).to eq(2)
       expect(pawn.position_y).to eq(2)
-      expect(king.position_x).to be_nil
       expect(king.position_y).to be_nil
-    end  
+      expect(king.position_x).to be_nil
+    end
+  end
+
+  def king
+    board.pieces.find_by(type: "King", color: "t")
+  end
 end
